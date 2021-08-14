0fa18a9ba3a2842cc6b3bc37ff7355b522d0bd0f
Sho Kurosu <xsho0212@outlook.com>
2021-08-14 16:35:27 +0900

    add: so_long.h

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
-#include <mlx.h>
+#include "so_long.h"
 
 int	main(void)
 {
-	void *mlx;
-	void *window;
+	t_solong so_long;
 
-	mlx = mlx_init();
-	window = mlx_new_window(mlx, 800, 600, "minilibx window");
-	mlx_loop(mlx);
+	so_long.mlx = mlx_init();
+	so_long.window = mlx_new_window(so_long.mlx, 800, 600, "minilibx window");
+	mlx_loop(so_long.mlx);
 }
---
+++ so_long.h
@@ -0,0 +1,12 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include <mlx.h>
+
+typedef struct	s_solong
+{
+	void*	mlx;
+	void*	window;
+}				t_solong;
+
+#endif
